3c0ef1960146b426d54418a25bd2397b72ef8ecf
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-01-22 14:10:53 -0500

    forLoop

---
+++ forLoop.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main() {
+    puts("\n");
+    int i;
+    for (i = 1; i <= 5; i++)
+    {
+        printf("Arun\n");
+    }
+    printf ("%d", i);
+    puts("\n");
+    return 0;
+}
