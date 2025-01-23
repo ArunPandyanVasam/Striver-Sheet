e9165f4c6f1ee368574b2853ee026f60f309002f
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-01-22 21:27:24 -0500

    rightAngledNumberPyramid

---
+++ rightAngledNumberPyramid.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+
+void printRectangularStarPattern(int n)
+{
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d ", j);
+        }
+        puts(" ");
+    }
+}
+
+int main()
+{
+    puts("\n");
+
+    int n, m;
+    printf("Input the value of n and m(give space between each value using space/tab button): ");
+    scanf("%d %d", &n, &m);
+    puts(" ");
+    printRectangularStarPattern(n);
+    puts(" ");
+    printRectangularStarPattern(m);
+
+    puts("\n");
+    return 0;
+}
