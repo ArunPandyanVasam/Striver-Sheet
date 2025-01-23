20127e391d3b63d3a3c2749390ed5a7d24fe5f75
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-01-23 15:29:32 -0500

    diamondStarPattern

---
+++ diamondStarPattern.c
@@ -0,0 +1,71 @@
+
+#include <stdio.h>
+
+void printStarPyramidPattern(int n)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        //space
+        for (int j = 0; j < n- i - 1; j++)
+        {
+           printf(" ");
+        }
+        
+
+        //star
+        for (int j = 0; j < 2*i+1; j++)
+        {
+            printf("*");
+        }
+
+        //space
+        for (int j = 0; j < n- i - 1; j++)
+        {
+            printf(" ");
+        }
+        puts("");
+    }
+    
+}
+
+void printInvertedStarPyramidPattern(int n)
+{
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        // space
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            printf(" ");
+        }
+
+        // star
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            printf("*");
+        }
+
+        // space
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            printf(" ");
+        }
+        puts("");
+    }
+}
+
+int main()
+{
+    puts("\n");
+
+    int n;
+    printf("Input the value of n: ");
+    scanf("%d", &n);
+    puts(" ");
+    printStarPyramidPattern(n);
+    printInvertedStarPyramidPattern(n);
+
+    puts("\n");
+    return 0;
+}
