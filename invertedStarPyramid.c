256eadb2ae255df79c382a9b301425d66a68671a
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-01-23 15:10:41 -0500

    invertedStarPyramid

---
+++ invertedStarPyramid.c
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+
+void printStarPyramidPattern(int n)
+{
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        // space
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            printf(" ");
+        }
+
+        // star
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            printf("*");
+        }
+
+        // space
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            printf(" ");
+        }
+        puts("");
+    }
+}
+
+int main()
+{
+    puts("\n");
+
+    int n;
+    printf("Input the value of n: ");
+    scanf("%d", &n);
+    puts(" ");
+    printStarPyramidPattern(n);
+
+    puts("\n");
+    return 0;
+}
