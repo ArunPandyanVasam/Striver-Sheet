928061d54499cba9596c5ce6d869a5d3224803a4
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-01-25 13:37:12 -0500

    theNumberPattern

---
+++ theNumberPattern.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define min(a, b) ((a) < (b) ? (a) : (b)) // macros new concept
+
+/*
+    int a = 10, b = 20, c = 5;
+    int min = (a < b) ? (a < c ? a : c) : (b < c ? b : c);
+    printf("Minimum value: %d\n", min);
+*/
+
+void printtheNumberPattern(int n)
+{
+
+    for (int i = 0; i < 2 * n - 1; i++)
+    {
+        for (int j = 0; j < 2 * n - 1; j++)
+        {
+            int top = i;
+            int left = j;
+            int bottom = 2 * n - 2 - i;
+            int right = 2 * n - 2 - j;
+
+            printf("%d", n - min(min(top, bottom), min(left, right)));
+        }
+
+        puts(" ");
+    }
+}
+
+int main()
+{
+    puts("\n");
+
+    int n;
+    printf("Input the value of n: ");
+    scanf("%d", &n);
+    puts(" ");
+    printtheNumberPattern(n);
+
+    puts("\n");
+    return 0;
+}
