dc8dd156c2a409f0b29d0ec85028eb7675948405
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-01-26 16:04:25 -0500

    countDigits

--- countDigits.c
+++ countDigits.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 void printCountDigits(int n)
 {
     int count = 0;
+    int original = n;  // Store the original value of n for later use
+
     printf("\nDigits in a number: ");
     while (n > 0)
     {
-        int lastDigit = n%10;
+        int lastDigit = n % 10;
         printf("%d ", lastDigit);
-        n = n/10;
+        n = n / 10;
         count = count + 1;
     }
 
     printf("\nNumber of digits in a number: %d", count);
-    
+
+    if (original > 0) {
+        int counter = (int)(log10(original) + 1);
+        printf("\nCounter: %d", counter);
+    } else {
+        printf("\nCounter: 1");  // Handling the case when n = 0
+    }
 }
 
 int main()
