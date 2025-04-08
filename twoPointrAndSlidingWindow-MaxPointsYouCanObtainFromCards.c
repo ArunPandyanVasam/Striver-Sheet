8e5234030e27f5da85a72816d514a0dae3001f5a
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-04-08 08:00:41 -0400

    twoPointrAndSlidingWindow-MaxPointsYouCanObtainFromCards

--- twoPointrAndSlidingWindow-MaxPointsYouCanObtainFromCards.c
+++ twoPointrAndSlidingWindow-MaxPointsYouCanObtainFromCards.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
+/*
+Time Complexity: O(k)
+- First loop runs k times to calculate lSum.
+- Second loop runs k times to shift left and add right elements.
+
+Space Complexity: O(1)
+- Constant space is used regardless of input size.
+*/
+
 int maxPoints(int arr[], int n, int k)
 {
-
     int lSum = 0, rSum = 0, maxSum = 0;
 
     for (int i = 0; i < k; i++)
     {
-        lSum = lSum + arr[i];
+        lSum = lSum + arr[i];  // O(k)
     }
 
     maxSum = lSum;
@@ -15,23 +23,23 @@ int maxPoints(int arr[], int n, int k)
     int rIndex = n - 1;
     for (int i = k - 1; i >= 0; i--)
     {
-        lSum = lSum - arr[i];
+        lSum = lSum - arr[i];  // O(k)
         rSum = rSum + arr[rIndex];
-        rIndex = rIndex + 1;
+        rIndex = rIndex - 1;
         maxSum = (lSum + rSum > maxSum) ? lSum + rSum : maxSum;
     }
+
     return maxSum;
 }
 
 int main(void)
 {
-
     int arr[] = {6, 2, 3, 4, 7, 2, 1, 7, 1};
     int n = sizeof(arr) / sizeof(arr[0]);
     int k = 4;
 
     int result = maxPoints(arr, n, k);
-    printf("Maximum Points You Can Obtain from Cards: %d", result);
+    printf("Maximum Points You Can Obtain from Cards: %d\n", result);
 
     return 0;
 }
