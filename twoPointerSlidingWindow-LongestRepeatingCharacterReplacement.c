6d72a072ddd379cdc771ec0a444e44b78601beae
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-04-26 21:56:36 -0400

    twoPointerSlidingWindow-LongestRepeatingCharacterReplacement

--- twoPointerSlidingWindow-LongestRepeatingCharacterReplacement.c
+++ twoPointerSlidingWindow-LongestRepeatingCharacterReplacement.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <string.h>
+
+void longestRepeatingCharacterReplacement_Brute(char *str, int n) {
+    
+}
 
 int main(void) {
+    char str[] = "AABABBA";
+    int n = strlen(str);
+
+    longestRepeatingCharacterReplacement_Brute(str, n);
 
     return 0;
 }
